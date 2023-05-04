1a0254a34560b072f4e0d523229ba96d15f38349
pinkir <pinki.roy@outlook.com>
2023-05-04 17:28:31 +0600

    hide firebase

--- src/PrivateRoute/PrivateRoute.jsx
+++ src/PrivateRoute/PrivateRoute.jsx
@@ -6,7 +6,7 @@ import { Spinner } from 'react-bootstrap';
 const PrivateRoute = ({children}) => {
     const {user, loading} = useContext(AuthContext);
     const location = useLocation();
-    console.log(user);
+    // console.log(user);
 
     if(loading){
         return <Spinner animation="border" variant="warning" />
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,15 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+console.log('env' , import.meta.env.VITE_apikey)
 const firebaseConfig = {
-  apiKey: "AIzaSyAxlZAw2Jin1prs8hWFy4H9atA9V-sdvCA",
-  authDomain: "chefs-dining.firebaseapp.com",
-  projectId: "chefs-dining",
-  storageBucket: "chefs-dining.appspot.com",
-  messagingSenderId: "337246457641",
-  appId: "1:337246457641:web:bd422852cd350fbf0e3138"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STROAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase

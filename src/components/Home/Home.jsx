07de9afec4fe5278525c77df83d8f9f0836fa57b
pinkir <pinki.roy@outlook.com>
2023-05-05 17:45:39 +0600

    pdf button worked

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -20,25 +20,25 @@ const Home = () => {
             <Carousel>
                 <Carousel.Item>
                     <img
-                        className="d-block w-100 overlay"
+                        className="d-inline w-50 overlay"
                         src={slider3}
                         alt="First slide"
                     />
-                    <Carousel.Caption className='mb-2'>
-                        <h3 className='fs-1 fw-bold'>Welcome to <span className='text-dark '>Chef's</span> Dining</h3>
-                        <p className='fw-semibold'>Here you can find so many signature recipes from the best chef's. <br /> Indulge in the flavors of the Mediterranean - Experience the richness of culture and cuisine!</p>
+                    <Carousel.Caption className=' text-end'>
+                        <h3 className='fs-2 fw-bold'>Welcome to <span className='text-dark '>Chef's</span> Dining</h3>
+                        <p className='fw-semibold'>Here you can find so many signature recipes from <br />the best chef's. Indulge in the flavors of the <br /> <span className='text-dark fw-bold'>Mediterranean - Experience</span> <br /> the richness of culture and cuisine!</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
-                        className="d-block w-100"
+                        className="d-block w-50"
                         src={slider2}
                         alt="Second slide"
                     />
 
-                    <Carousel.Caption className='mb-2'>
-                        <h3 className='fs-1 fw-bold'>Welcome to <span className=''>Chef's</span> Dining</h3>
-                        <p className='fw-semibold'>Here you can find so many signature recipes from the best chef's.</p>
+                    <Carousel.Caption className=' text-end text-dark'>
+                        <h3 className='fs-2 fw-bold'>Mediterranean cuisine in <br /> <span className='text-light'>Chef's</span> Dining</h3>
+                        <p className='fw-semibold'>Mediterranean cuisine is known for its use of fresh and <br /> flavorful ingredients like olive oil, herbs, and vegetables <br /> as well as dishes that feature seafood, <br /> legumes, grains, and lean proteins like chicken and lamb.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
@@ -48,7 +48,7 @@ const Home = () => {
 
             </div>
             <div className='pt-5 mt-5'>
-                <div className='about text-center p-5 m-5'>
+                <div className='about text-center p-5 m-5 img-fluid'>
                     <i><h3 className='m-5 fw-bold'>About us</h3>
                         <p className='fw-sm'>We are passionate about sharing the flavors, culture, and history of the Mediterranean <br /> region with the world. Our blog features a variety of articles, recipes, and resources <br /> to help you discover the rich and diverse culinary traditions of countries such as <br />Greece, Italy, Turkey, Spain, and Morocco. <br /> Discover the vibrant flavors of the Mediterranean with our authentic recipes, crafted with the freshest ingredients <br /> and time-honored techniques.  From savory seafood dishes to hearty vegetarian options, our diverse menu will <br /> transport your taste buds to the sun-kissed shores of Greece, Italy, and beyond. <br /> Come explore the culinary wonders of the Mediterranean today!</p></i>
 
